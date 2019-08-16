c596bf121d707d12eb12ccf2f1320eb1306a50e7
Nitya Hoyos <tronsymphony@gmail.com>
2019-08-15 17:02:21 -0700

    reactjs

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from 'react';
+
+function About(){
+	return(
+		<React.Fragement>
+			<h1>About</h1>
+			<p>this is the todolist app</p>
+		</React.Fragement>
+	)
+}
+
+export default Component;
